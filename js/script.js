6ada9b0f0f292d4544355b7454d9aea17cdd328d
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-08 17:52:39 +0200

    mancano calcoli percentuale

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ let prezzoBiglietto = (KmDaPercorrere * prezzoFissoBiglietto);
 
 console.log("Il prezzo del biglietto è: " + prezzoBiglietto + "Euro")
 
+if(etaPasseggero < 18){
+    prezzoBiglietto *= 0.79;
+}
+else if(etaPasseggero >= 65){
+    prezzoBiglietto *= 0.58;
+}
+
+let prezzoFinale= prezzoBiglietto .toFixed(2);
+console.log("Il prezzo finale del biglietto con lo sconto è:" + prezzoFinale + "Euro");
+
