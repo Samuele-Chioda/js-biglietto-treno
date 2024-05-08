2aa33c931654a0ce361a15986c9eb919fdb5e3a0
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-08 17:21:34 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let KmDaPercorrere = parseInt(prompt("Inserisci i chilomentri che desideri percorrere:"));
+
+let etaPasseggero = parseInt(prompt("Inserisci l'età del tuo passeggero"));
+
+let prezzoBiglietto = (KmDaPercorrere * 0.276);
